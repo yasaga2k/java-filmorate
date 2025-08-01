ff1b2b4a844c4c273fef3c921112fb88e872d799
Egor Zauzolkin <qkkkkkkkkkkkk@yandex.ru>
2025-08-01 13:35:56 +0300

    Доработал замечания, добавил валидатор для кастомной аннотации

---
+++ src/main/java/ru/yandex/practicum/filmorate/customannotation/ReleaseDate.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.customannotation;
+
+public class ReleaseDate {
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/ReleaseDateValidator.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.filmorate.validator;
+
+public class ReleaseDateValidator {
+}
