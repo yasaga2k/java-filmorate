288d5271681d1a68f3d4386b2ab977e4b8e5e529
Kirill-Bryansk <k.mashnov@yandex.ru>
2025-09-26 16:01:05 +1000

    Изменил метод update

--- src/main/java/ru/yandex/practicum/filmorate/storage/dao/DirectorDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/dao/DirectorDbStorage.java
@@ -8,7 +8,6 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Director;
 
 import java.sql.PreparedStatement;
